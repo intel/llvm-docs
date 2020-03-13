afefd2f2810b022bdce37129633228a1c0fa0ad2
iclsrc <ia.compiler.tools.git@intel.com>
2020-03-13 01:08:49 +0000

    Update docs
    Signed-off-by: iclsrc <ia.compiler.tools.git@intel.com>

--- search/functions_b.js
+++ search/functions_b.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['maketraceeventprolog',['makeTraceEventProlog',['../classcl_1_1sycl_1_1detail_1_1Command.html#a94b8d77da7c09799a1b9913639fcc31d',1,'cl::sycl::detail::Command']]],
-  ['mem_5fadvise',['mem_advise',['../classcl_1_1sycl_1_1queue.html#a4d2a9b32cd6237e4b6573d816dd299f9',1,'cl::sycl::queue::mem_advise()'],['../classcl_1_1sycl_1_1detail_1_1queue__impl.html#aea93fc878cfe03d14eacedab423368b1',1,'cl::sycl::detail::queue_impl::mem_advise()']]],
+  ['mem_5fadvise',['mem_advise',['../classcl_1_1sycl_1_1queue.html#acdf855d45f7c048c839e283c036a05cb',1,'cl::sycl::queue::mem_advise()'],['../classcl_1_1sycl_1_1detail_1_1queue__impl.html#a64a2a8cf81ca5f32dec6948e22404209',1,'cl::sycl::detail::queue_impl::mem_advise()']]],
   ['mem_5ffence',['mem_fence',['../classcl_1_1sycl_1_1group.html#a3323de92859b597a8ca6be50235296fa',1,'cl::sycl::group::mem_fence()'],['../classcl_1_1sycl_1_1nd__item.html#a38ea2d76be164ddbbf17355814f26874',1,'cl::sycl::nd_item::mem_fence()']]],
   ['memcpy',['memcpy',['../classcl_1_1sycl_1_1handler.html#a110067774f86c17741d3dbcaba51de0c',1,'cl::sycl::handler::memcpy()'],['../classcl_1_1sycl_1_1queue.html#a6bc6a510e5e9abcbf1ee904d4b86edbf',1,'cl::sycl::queue::memcpy()'],['../classcl_1_1sycl_1_1detail_1_1queue__impl.html#aba5150e8132fdbaa945fdc59102639fb',1,'cl::sycl::detail::queue_impl::memcpy()']]],
   ['memset',['memset',['../classcl_1_1sycl_1_1handler.html#abacc9b24930b0343613e9746734ec540',1,'cl::sycl::handler::memset()'],['../classcl_1_1sycl_1_1queue.html#afcd93967ba2484d50786c22b4148cb67',1,'cl::sycl::queue::memset()'],['../classcl_1_1sycl_1_1detail_1_1queue__impl.html#a512fca4ad31e7ae4c97a0223dabf038e',1,'cl::sycl::detail::queue_impl::memset()']]]
