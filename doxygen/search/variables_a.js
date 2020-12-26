81b5d4b5b2a5ca3fae34422bc3be97d511d0449a
iclsrc <ia.compiler.tools.git@intel.com>
2020-12-26 02:08:53 +0000

    Update docs
    Signed-off-by: iclsrc <ia.compiler.tools.git@intel.com>

--- doxygen/search/variables_a.js
+++ doxygen/search/variables_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['kernelparamoptinfo',['KernelParamOptInfo',['../classcl_1_1sycl_1_1detail_1_1pi_1_1DeviceBinaryImage.html#a5ce259269c04321089a8a1cae77654fb',1,'cl::sycl::detail::pi::DeviceBinaryImage']]],
-  ['kind',['kind',['../structcl_1_1sycl_1_1detail_1_1kernel__param__desc__t.html#ab02bfda6fa8097e2a80f0cfc01fef305',1,'cl::sycl::detail::kernel_param_desc_t::kind()'],['../structpi__device__binary__struct.html#aecd218b85c8c7499aa5c22e900de30dc',1,'pi_device_binary_struct::Kind()']]],
+  ['kind',['Kind',['../structpi__device__binary__struct.html#aecd218b85c8c7499aa5c22e900de30dc',1,'pi_device_binary_struct::Kind()'],['../structcl_1_1sycl_1_1detail_1_1kernel__param__desc__t.html#ab02bfda6fa8097e2a80f0cfc01fef305',1,'cl::sycl::detail::kernel_param_desc_t::kind()']]],
   ['kind_5f',['kind_',['../struct__pi__context.html#aacdb938710dcd43e1f12123af4043da3',1,'_pi_context']]]
 ];
