a65631e4092bab808803c47e467c26a67b29c2a4
iclsrc <ia.compiler.tools.git@intel.com>
2021-01-28 02:04:23 +0000

    Update docs
    Signed-off-by: iclsrc <ia.compiler.tools.git@intel.com>

--- doxygen/struct__pi__ze__event__list__t.js
+++ doxygen/struct__pi__ze__event__list__t.js
@@ -1,8 +1,8 @@
 var struct__pi__ze__event__list__t =
 [
+    [ "collectEventsForReleaseAndDestroyPiZeEventList", "struct__pi__ze__event__list__t.html#a8deee13a15eece8117dc540043e3c9ad", null ],
     [ "createAndRetainPiZeEventList", "struct__pi__ze__event__list__t.html#a5688967d2c82ead816dfb93154a11346", null ],
     [ "operator=", "struct__pi__ze__event__list__t.html#a3b941d06364e85c0058fd727e8d3d1e0", null ],
-    [ "releaseAndDestroyPiZeEventList", "struct__pi__ze__event__list__t.html#adff70995dd82eddb20e6fdcf8d3cfea7", null ],
     [ "Length", "struct__pi__ze__event__list__t.html#a6ef7a55b77f2837247186ff805100e2b", null ],
     [ "PiEventList", "struct__pi__ze__event__list__t.html#a5b2c42fd19476bf1a179ebb16ac1fce8", null ],
     [ "PiZeEventListMutex", "struct__pi__ze__event__list__t.html#a367b565957f8d976faec6135b7c4a506", null ],
