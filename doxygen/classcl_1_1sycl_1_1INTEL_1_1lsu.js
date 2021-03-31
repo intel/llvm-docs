21e1a2f0d49b369ddf3d6d25b5615fe1b2a752ad
iclsrc <ia.compiler.tools.git@intel.com>
2021-03-31 01:31:27 +0000

    Update docs
    Signed-off-by: iclsrc <ia.compiler.tools.git@intel.com>

--- doxygen/classcl_1_1sycl_1_1INTEL_1_1lsu.js
+++ doxygen/classcl_1_1sycl_1_1INTEL_1_1lsu.js
@@ -1,6 +1,6 @@
 var classcl_1_1sycl_1_1INTEL_1_1lsu =
 [
     [ "lsu", "classcl_1_1sycl_1_1INTEL_1_1lsu.html#a870cac9e726f4b6712e006bd5c922e45", null ],
-    [ "load", "classcl_1_1sycl_1_1INTEL_1_1lsu.html#af87009d10b421ddb62760d545f6beba8", null ],
-    [ "store", "classcl_1_1sycl_1_1INTEL_1_1lsu.html#a352698bdc09f21511a9a2b06a3ac406a", null ]
+    [ "load", "classcl_1_1sycl_1_1INTEL_1_1lsu.html#a04da5f26e4e82551dd5e61c2cb99eae0", null ],
+    [ "store", "classcl_1_1sycl_1_1INTEL_1_1lsu.html#a2706a0276cea4752f6db77ffa7959749", null ]
 ];
